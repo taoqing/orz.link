795aa3552f65e7242da83af40c48e4fdec02de1d
taoqing <phpdiy@gmail.com>
2014-04-25 17:18:33 +0800

    index file

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width" />
+<meta name="keywords" content="orz.link" />
+<meta name="description" content="orz.link" />
+<title>orz.link</title>
+<link href="css/global.css" rel="stylesheet" />
+<script src="js/global.js"></script>
+</head>
+
+<body>
+<div id="placeholder">
+    <?php echo "\r\n"; ?>
+</div>
+</body>
+</html>
---
+++ js/global.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+(function(){
+	if(navigator.appName === 'Microsoft Internet Explorer'){
+		alert("Sorry, This website does not support your browser, Please download the following browser add-on");
+		window.location = 'http://www.google.com/chromeframe/eula.html';
+	}
+})();
